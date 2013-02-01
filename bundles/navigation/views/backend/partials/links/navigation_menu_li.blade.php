e3ade6caa329a8a97d63177b0d582adc1904090f
Myopengrid <myopengrid@gmail.com>
2013-01-31 19:51:44 -0600

    Prevent navigation link class to be applied to the backend view

--- bundles/navigation/views/backend/partials/links/navigation_menu_li.blade.php
+++ bundles/navigation/views/backend/partials/links/navigation_menu_li.blade.php
@@ -1,6 +1,6 @@
 <li id="{{ $child['li_id'].$child['id'] }}">
     <div class="section-link">
-        <a class="{{ $child['class'] }}" {{ empty($child['target']) ? '' : 'target="_blank"' }} href="{{ $url }}" alt="{{ $child['id'] }}" rel="{{ $child['rel'] }}">{{ Str::title($child['title']) }}</a>
+        <a {{ empty($child['target']) ? '' : 'target="_blank"' }} href="{{ $url }}" alt="{{ $child['id'] }}" rel="{{ $child['rel'] }}">{{ Str::title($child['title']) }}</a>
     </div>
     @if(isset($child['children']) and !empty($child['children']))
     <ul>
