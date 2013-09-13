7e4492a90c6b81cfce18935f68d7fc2acbb26349
Jefferson Costella <jefersonc@gmail.com>
2013-09-13 18:16:38 -0500

    Updates page slug

--- bundles/pages/views/duplicate.blade.php
+++ bundles/pages/views/duplicate.blade.php
@@ -78,7 +78,7 @@
         <div class="control-group input-prepend">
           {{ Form::label('slug', 'Slug', array('class' => 'control-label')) }}
           <div class="controls">
-            <span class="add-on span1">{{URL::base()}}/</span>
+            <span class="add-on span1">{{URL::base()}}/page/</span>
             {{ Form::text('slug', Input::old('slug'), array('class' => 'span2')) }}
             <span class="help-inline">{{ $errors->has('slug') ? $errors->first('slug') : '' }}</span>
           </div>
